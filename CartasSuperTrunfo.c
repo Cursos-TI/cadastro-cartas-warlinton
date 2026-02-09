a831086db06d2e90e56451c0f6dad6c576af5818
warlinton <warliton33@gmail.com>
2026-02-09 15:11:29 +0000

    Calcular

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,18 @@ int População = 12325000;
 float Àrea = 1521.11;
 float PIB = 699.28;
 int Pontos_Turísticos = 50;
+
+float populacao1 = 12325000;
+float area1 = 1521.11;
+float PIB1 = 699280000000.00;
+float Densidade_Populacional1 = populacao1 / area1; 
+float PIB_per_Capita1 = PIB1 / populacao1;
+
+float populacao2 = 6748000;
+float area2 = 1200.25;
+float PIB2 = 300500000000.00;
+float Densidade_Populacional2 = populacao2 / area2; 
+float PIB_per_Capita2 = PIB2/ populacao2;
    
 printf("Titulo:  %s\n", nome);
 printf("Estado: %c\n",  Estado);
@@ -18,6 +30,8 @@ printf("População: %d\n", População);
 printf("Àrea em km²: %.2f\n",  Àrea);
 printf("PIB em Bilhões de reais: %.2f\n",  PIB);
 printf("Número de Pontos Turísticos:  %d\n",  Pontos_Turísticos);
+printf("Densidade Populacional em hab/km²: %.2f\n", Densidade_Populacional1);
+printf("PIB per Capita em reais: %.2f\n", PIB_per_Capita1);
 printf(" \n");
 
 
@@ -39,10 +53,21 @@ printf("População: %d\n", população);
 printf("Àrea em km²: %.2f\n",  area);
 printf("PIB em Bilhões de reais: %.2f\n",  Pib);
 printf("Número de Pontos Turísticos:  %d\n",  pontos_Turisticos);
+printf("Densidade Populacional em hab/km²: %.2f\n", Densidade_Populacional2);
+printf("PIB per Capita em reais: %.2f\n", PIB_per_Capita2);
 printf(" \n");
 
 
+
+
 return 0;
-  
+ 
+
+
+
+
+
+
+
 
 }
